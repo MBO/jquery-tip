0db7bd5232519e25a1135d3c77e8115ed10a0bfd
MBO <boruta.miroslaw+git@gmail.com>
2010-05-12 10:44:54 +0200

    Add actual state of work on jquery-tip plugin

---
+++ jquery.tip.js
@@ -0,0 +1,147 @@
+/**
+ * Author: Mirosław Boruta <boruta.miroslaw@gmail.com>
+ *
+ * Functions:
+ *   open
+ *   close
+ *   widget
+ *   destroy
+ * Events:
+ *   open
+ *   close
+ * Options:
+ *   autoOpen[true] - if this tip should be open after create
+ *   text["INSERT YOUR OWN TIP HERE!"] - text to be displayed inside tip can be:
+ *      string - insert string inside tip
+ *      jQuery/Node element - append inside tip
+ *      function - call in context of actual element and take resulting string or jQuery/Node element
+ *   closeText["close"] - text to place in close link
+ *   tipClass[""] - additional classes to add to tip widget
+ *   zIndex[1000] - css z-index property for tip widget
+ *   hide[null] - jquery-type duration for hide animation
+ *   show[null] - jqyery-type duration for show animation
+ *   position["right"] - type of position for widget, possible values:
+ *          topleft  +---------+  topright
+ *             left  | element |  right
+ *       bottomleft  +---------+  bottomright
+ *   horizontalOffset[0] - number of pixels to move tip widget border out of element
+ */
+(function($){
+
+var uiTipClasses =
+  "ui-tip " +
+  "ui-widget " +
+  "ui-widget-content " +
+  "ui-corner-all ";
+
+$.widget("ui.tip", {
+    options: {
+        autoOpen: true,
+        text: "INSERT YOUR OWN TIP HERE!",
+        closeText: "close",
+        tipClass: "",
+        zIndex: 1000,
+        hide: null,
+        show: null,
+        position: 'right',
+        horizontalOffset: 0
+    },
+    _create: function() {
+        var self = this,
+            options = self.options;
+
+        var uiTip = (self.uiTip = $("<div/>"))
+                .appendTo(document.body)
+                .hide()
+                .addClass(uiTipClasses + options.tipClass + " ui-tip-"+options.position)
+                .css({ zIndex: options.zIndex }),
+
+            uiTipContent = $("<div/>")
+                .append($.isFunction(self.options.text)
+                    ? self.options.text.call(self.element)
+                    : self.options.text)
+                .addClass("ui-tip-content ui-widget-content")
+                .appendTo(uiTip),
+
+            uiTipTitlebar = $("<div/>")
+                .addClass(
+                    "ui-tip-titlebar " +
+                    "ui-widget-header " +
+                    "ui-corner-all " +
+                    "ui-helper-clearfix")
+                .prependTo(uiTip),
+
+            uiTipTitlebarClose = $('<a href="#"></a>')
+                .addClass(
+                    "ui-tip-close " +
+                    "ui-corner-all")
+                .hover(
+                    function() { uiTipTitlebarClose.addClass("ui-state-hover"); },
+                    function() { uiTipTitlebarClose.removeClass("ui-state-hover"); })
+                .focus(function() { uiTipTitlebarClose.addClass("ui-state-focus"); })
+                .blur(function() { uiTipTitlebarClose.removeClass("ui-state-focus"); })
+                .mousedown(function(event) { event.stopPropagation(); })
+                .click(function(event) {
+                    self.close(event);
+                    return false; })
+                .appendTo(uiTipTitlebar),
+
+            uiTipTitlebarCloseText = $("<span/>")
+                .addClass(
+                    "ui-icon " +
+                    "ui-icon-closethick")
+                .text(options.closeText)
+                .appendTo(uiTipTitlebarClose);
+
+    },
+    _init: function() {
+        if (this.options.autoOpen) {
+            this.open();
+        }
+    },
+    widget: function() {
+        return this.uiTip;
+    },
+    destroy: function() {
+        var self = this;
+        self.uiTip.hide();
+        self.uiTip.remove();
+        return self;
+    },
+    _position: function() {
+        var topOffset;
+        var leftOffset;
+        if (this.options.position.search(/left$/) > -1) {
+            leftOffset = this.element.offset().left - this.uiTip.outerWidth() - this.options.horizontalOffset;
+        } else {
+            leftOffset = this.element.offset().left + this.element.outerWidth() + this.options.horizontalOffset;
+        }
+        if (this.options.position.search(/^top/) > -1) {
+            topOffset = this.element.offset().top;
+        } else if (this.options.position.search(/^bottom/) > -1) {
+            topOffset = this.element.offset().top + this.element.outerHeight() - this.uiTip.outerHeight();
+        } else {
+            topOffset = this.element.offset().top + (this.element.outerHeight() - this.uiTip.outerHeight())/2;
+        }
+        
+        this.uiTip.css({
+            position: "absolute",
+            top: topOffset,
+            left: leftOffset
+        });
+    },
+    open: function() {
+        var self = this;
+        self._position();
+        self.uiTip.show(self.options.show, function() { self._trigger("open") });
+        return self;
+    },
+    close: function() {
+        var self = this;
+        self.uiTip.hide(self.options.hide, function() { self._trigger("close") });
+        return self;
+    }
+});
+
+})(jQuery);
+
